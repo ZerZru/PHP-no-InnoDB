8eb6bd017af0d3bfff9b3fefd359740557905edc
Elisey Sharov <30829308+ZerZru@users.noreply.github.com>
2018-03-29 19:29:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,25 @@
 <?php
     session_start();
-    ?>
-    <html>
+?>
+<html>
     <head>
-    <title>Home page</title>
+        <title>Home page</title>
     </head>
     <body>
-    <h2>Home page</h2>
-    <form action="login.php" method="post">
-<a href="signup.php">Registration</a> 
-    </p></form>
-    <br>
-    <?php
-        if (empty($_SESSION['login']) or empty($_SESSION['id'])) {
-            echo "Welcome, Guest!<br><a href='#'>Only registered users</a>
-             <p>
-            <label>Login:<br></label>
-            <input name='login' type=text' maxlength='32'>
-            </p>
-            <p>
-
-            <label>Password:<br></label>
-            <input name='password' type='password' maxlength='32'>
-            </p>
-            <p>
-            <input type='submit' name='submit' value='Login'><br>"; 
-        } else {
-            echo "Welcome, ".$_SESSION['login']."<br><a href='http://t.me/scg_now'>123</a>";
-        }
-    ?>
+        <h2>Home page</h2>
+        <form action="login.php" method="post">
+            <a href="signup.php">Registration</a> 
+        </form> <br>
+        <?php
+            if (empty($_SESSION['login']) or empty($_SESSION['id'])) {
+                echo "Welcome, Guest!<br><a href='#'>Only registered users</a><p><label>Login:<br></label>
+                <input name='login' type=text' maxlength='32'></p>
+                <p><label>Password:<br></label>
+                <input name='password' type='password' maxlength='32'></p>
+                <p><input type='submit' name='submit' value='Login'></p><br>"; 
+            } else {
+                echo "Welcome, ".$_SESSION['login']."<br><a href='http://t.me/scg_now'>123</a>";
+            }
+           ?>
     </body>
-    </html>
+</html>
