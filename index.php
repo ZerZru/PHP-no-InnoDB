7f45eb3ff2c0e7512952acdfee1803a9bffe9ce5
Elisey Sharov <30829308+ZerZru@users.noreply.github.com>
2018-10-01 11:20:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
     session_start();
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Home page</title>
     </head>
@@ -11,16 +12,16 @@
             <a href="signup.php">Registration</a> 
         </form> <br>
         <?php
-            if (empty($_SESSION['login']) or empty($_SESSION['id'])) { #empty session
+            if (empty($_SESSION['login']) or empty($_SESSION['id'])) { #if empty session
                 echo "Welcome, Guest!<br><a href='#'>Only for registered users</a><p><label>Login:<br></label>
                 <input name='login' type=text' maxlength='32'></p>
                 <p><label>Password:<br></label>
                 <input name='password' type='password' maxlength='32'></p>
                 <p><input type='submit' name='submit' value='Login'></p><br>"; 
-            } else { 
+            } else { #if user have session
                 echo "Welcome, ".$_SESSION['login']."<br><a href='https://github.com'>Your link</a>";
             }
            ?>
-        #page code
+        <!-- Page code -->
     </body>
 </html>
