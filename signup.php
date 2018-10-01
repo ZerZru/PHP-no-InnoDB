bf9fe7200009ffebdb0b132214fcb7639bf7d7b0
Elisey Sharov <30829308+ZerZru@users.noreply.github.com>
2018-10-01 11:25:09 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,7 +1,7 @@
 <form action="save_user.php" method="POST">
 	<p> Your login </p>
-	<input name="login" type="text" maxlength="32">
+	<input name="login" type="text" maxlength="32" required>
 	<p> Your password </p>
-	<input name="password" type="password" maxlength="32">
+	<input name="password" type="password" maxlength="32" required>
 	<input type="submit" name="submit" value="Sign up">
 </form>
