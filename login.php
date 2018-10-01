2a647764f65ad41092207c6709ebfefdfbedbb3c
Elisey Sharov <30829308+ZerZru@users.noreply.github.com>
2018-10-01 11:24:07 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,38 +1,29 @@
 <?php
     session_start();
-    if (isset($_POST['login'])) {
+    if (isset($_POST['submit'])) {
         $login = $_POST['login'];
-        if ($login == '') {
-            unset($login);
-        }
-    }
-    if (isset($_POST['password'])) { 
         $password=$_POST['password'];
-        if ($password =='') {
-            unset($password);
-        } 
     }
-    if (empty($login) or empty($password)) {
-        exit ("Insert all areas!");
-    }
+
     $login = stripslashes($login);
     $login = htmlspecialchars($login); #delete html-codes
     $password = stripslashes($password);
     $password = htmlspecialchars($password); #delete html-codes
-    $login = trim($login); #delete space
+    $login = trim($login); #delete spaces
+
     include ("db.php");
  
-    $result = mysql_query("SELECT * FROM users WHERE login='$login'",$db);
+    $result = mysql_query("SELECT * FROM `users` WHERE `login`='$login'", $db);
     $myrow = mysql_fetch_array($result);
     if (empty($myrow['password'])) {
         exit ("Incorrect login or password.");
     } else {
         if ($myrow['password']==$password) {
-    $_SESSION['login']=$myrow['login']; 
-    $_SESSION['id']=$myrow['id'];
-        echo "Ypu have authorised on site! <a href='index.php'>Главная страница</a>";
+            $_SESSION['login']=$myrow['login']; 
+            $_SESSION['id']=$myrow['id'];
+            
+            echo "You have authorised on site! <a href='index.php'>Main page</a>";
     } else {
         exit ("Incorrect login or password.");
-        }
     }
-    ?>
+?>
